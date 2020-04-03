1d80038d5fbde8f3b64389012da938e189734758
MoulanaMadani <mzmadani10@gmail.com>
2020-04-03 14:20:18 +0530

    done till home page with jobs

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-#class Blog(models.Models):
+class Blog(models.Model):
+
+    Title = models.CharField(max_length=200)
+    
+    Date = models.DateTimeField()
+    
+    Body = models.TextField(max_length=750)
+    
+    image = models.ImageField(upload_to='images/')
--- jobs/admin.py
+++ jobs/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Jobs
+from .models import Job
 
-admin.site.register(Jobs)
+admin.site.register(Job)
